af61ca82c28de644d7a27381bb06c1db82e56fbe
kubilaycicek <kubilaycicek0693@gmail.com>
2020-03-19 20:48:03 +0300

    update login

--- mylib/src/main/java/com/kubilaycicek/mylib/controller/LoginController.java
+++ mylib/src/main/java/com/kubilaycicek/mylib/controller/LoginController.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -14,7 +13,6 @@ public class LoginController {
     public String login() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (!(auth instanceof AnonymousAuthenticationToken)) {
-            System.out.println("deneme");
             return "/book/book-list";
         }
         return "login";
